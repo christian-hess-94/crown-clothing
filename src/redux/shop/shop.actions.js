9cb50eb1a824b160890a32a190449d7f90ccb5b9
Christian Hess <christianhess94@gmail.com>
2020-08-07 10:32:06 -0300

    Lesson 185 (Starting to add sagas)

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -14,6 +14,15 @@ const fetchCollectionsFailure = (errorMessage) => ({
     payload: errorMessage
 })
 
+/**
+ * Função a ser usada pelo Redux-Thunk
+ * 
+ * Redux-thunk reconhece quando a Action retorna uma function e passa o parâmetro dispatch do Redux como primeiro argumento
+ * Esse parametro dispatch é usado dentro da função para disparar Actions ao Redux que alteram o state em relação a execução
+ * 
+ * Apenas funciona se redux-thunk for um middleware ativado. 
+ * Caso não haja redux-thunk, a Actions apenas podem retornar ActionObject que possuem os parâmetros "type" e "payload".
+ */
 export const fetchCollectionsStartAsync = () => dispatch => {
     const collectionRef = firestore.collection("collections");
     dispatch(fetchCollectionsStart())
--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,6 @@ const middlewares = [thunk];
 if (process.env.NODE_ENV === 'development') {
     // middlewares.push(logger)
 }
-
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
 export const persistor = persistStore(store)
