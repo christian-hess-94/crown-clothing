a7b3080fa21f3544fdc9ee5c640fa3ddf9a5f4fc
Christian Hess <christianhess94@gmail.com>
2020-09-01 13:44:27 -0300

    Adicionado teste basico

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { store, persistor } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('React is rendered', () => {
+    const { baseElement } = render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <PersistGate persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </Provider>
+        </BrowserRouter>
+    );
+    //   const linkElement = getByText(/learn react/i);
+    expect(baseElement).toBeInTheDocument();
 });
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider as ReduxProvider } from 'react-redux'
+import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store';
@@ -10,14 +10,13 @@ import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
     <React.StrictMode>
-        <ReduxProvider store={store}>
-            <BrowserRouter>
+        <BrowserRouter>
+            <Provider store={store}>
                 <PersistGate persistor={persistor}>
-
                     <App />
                 </PersistGate>
-            </BrowserRouter>
-        </ReduxProvider>
+            </Provider>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
